3dd67cbdd361d5c6204e4916772ddeb18f4f8a9f
BaranSaritasIbb <baran.saritasibb@gmail.com>
2024-08-02 11:40:00 +0300

    elastic ve eureka accounta kuuruldu test yapildi

--- src/main/java/com/SpringCloudApp/SpringCloudApp/accountService/src/main/java/com/SpringCloudApp/AccountServiceApplication.java
+++ src/main/java/com/SpringCloudApp/SpringCloudApp/accountService/src/main/java/com/SpringCloudApp/AccountServiceApplication.java
@@ -3,9 +3,11 @@ package com.SpringCloudApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.SpringCloudApp","com.ibb.boot.data"})
 public class AccountServiceApplication {
     public static void main(String[] args) {
